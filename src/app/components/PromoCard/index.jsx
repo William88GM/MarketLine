afd68abac163ee9ef057388b5a61f12c1e37c24f
Guillermo Martínez <guillermogabrielmartinezbarros@gmail.com>
2024-03-02 16:52:14 -0300

    much better

--- src/app/components/PromoCard/index.jsx
+++ src/app/components/PromoCard/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 export default function PromoCard({ arraySections }) {
   const price = 1000;
-  const tamaño = 10; //vh - Minimo 0
+  const tamaño = 10; //vh - Minimo 0 - El 0 se ve bien en todos lados menos en el Z Fold la descripcion del producto no se ve
   const separacion = 5; //vh
 
   return (
@@ -15,46 +15,50 @@ export default function PromoCard({ arraySections }) {
         minHeight: `${tamaño + 50}vh`,
         marginBottom: `${separacion}vh`,
         maxHeight: `${tamaño + 50}vh`,
+        backgroundColor: "#2A283D",
       }}
-      className={`  p-2  flex flex-col  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700`}
+      className={`  p-2  flex flex-col  rounded-lg `}
     >
       <h3 className="flex-1 [font-size:2.2vh] p-1 text-white ">
         Company title
       </h3>
-      <div className="flex-[9] flex  overflow-x-scroll gap-2 ">
+      <div className="flex-[9] flex scrollBar scroll-bar pb-2 overflow-x-scroll gap-2 ">
         {/*El overflow x ocupaba espacio en Y??? >:( */}
         {arraySections &&
           arraySections.products.map((element, i) => (
             <div
               key={i}
-              className={` flex flex-col gap-2 p-1 items-center justify-center`}
+              className={`rounded-lg flex flex-col gap-2 p-1 items-center justify-center`}
               style={{
+                backgroundImage:
+                  "linear-gradient(to left bottom, #2a283d, #2c2d44, #2e324b, #303753, #313c5a, #313c5a, #313c5a, #313c5a, #303753, #2e324b, #2c2d44, #2a283d)",
+
                 minWidth: `${tamaño / 2 + 25}vh`,
                 maxWidth: `${tamaño / 2 + 25}vh`,
               }}
             >
-              <h5 class="flex-1 [font-size:2.5vh] font-bold tracking-tight text-gray-900 dark:text-white">
+              <h5 class=" size-fit [font-size:2.5vh] font-bold tracking-tight text-gray-900 dark:text-white">
                 Recomendation Title
               </h5>
-              <div className="flex-[1]">
-                <img
-                  style={{ width: `${tamaño / 5 + 16}vh` }}
-                  className=" rounded-lg  "
-                  src={element.image}
-                  alt=""
-                />
-              </div>
-              {/* h-[12%] MEJOR*/}
-              <p className="max-h-[12%] mx-2 [font-size:2vh] overflow-hidden whitespace-nowrap text-ellipsis text-white text-pretty">
+
+              <img
+                style={{ width: `${tamaño / 5 + 16}vh` }}
+                className=" rounded-lg  "
+                src={element.image}
+                alt=""
+              />
+
+              <p className="onlyTwoLines mx-2  w-[80%] text-white  [font-size:2vh]">
                 El Mejor Suplemento de Colageno Hidrolizado, Flex, nutricion
                 Articular, En polvo, sabor citrico, en pote de 375g, vitalis,
                 navitas
               </p>
-              <span className="flex-[1] [font-size:2.2vh] text-white">
+
+              <span className="size-fit [font-size:2.5vh] text-white">
                 $ {price}
               </span>
-              <span className="flex-1 [font-size:1.7vh] text-white">
-                en 6x $ {(price / 6).toFixed(2)}{" "}
+              <span className="size-fit [font-size:2vh] text-white">
+                en 6x $ {(price / 6).toFixed(2)}
               </span>
             </div>
           ))}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="">
+      <body className="bg-slate-800">
         <header className="h-[8.5vh]">
           <Nav />
         </header>
